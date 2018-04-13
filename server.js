b4c36a3df2b34a71ff540ec7e978ded608ee31b4
Joshua Fredrickson <uafredrickson@gmail.com>
2018-04-12 21:36:10 -0700

    refactored Server code

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-var http = require ("http");
+var http = require ('http');
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World!");
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
     response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('Server has started');
